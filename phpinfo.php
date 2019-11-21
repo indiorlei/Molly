6b99e44957489cf7ebba10acf02e8ef17ae9b5c1
Indiorlei de Oliveira <indiorleioliveira@gmail.com>
2019-11-21 20:04:29 -0300

    test deploy

--- phpinfo.php
+++ phpinfo.php
@@ -1,1 +1,4 @@
-<?php phpinfo();
+<?php
+mysqli_connect('lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'v60er1qb6pnw3naj', 't012buheow0r9ado') or die(mysqli_error());
+echo "Connected to MySQL<br />";
+phpinfo();
