03ae9ea95f4e0d81c74f9c9258f63c337ab887f7
Indiorlei de Oliveira <indiorleioliveira@gmail.com>
2019-11-21 20:01:48 -0300

    test deploy

--- config.php
+++ config.php
@@ -23,6 +23,23 @@ error_reporting(E_ALL);
 //PDO
 function dbConnect()
 {
+
+    // correto
+    // try {
+    //     $db = new PDO("mysql:dbname=database;host=localhost;charset=utf8;", "root", "");
+    // } catch (PDOException $e) {
+    //     throw new PDOException($e);
+    // }
+
+    // errado
+    // try {
+    //     $db = new PDO("mysql:dbname=database;host=localhost;charset=utf8;", "root", "");
+    // } catch (PDOException $e) {
+    //     $e->getMessage();
+    // }
+
+
+
     try {
         $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -30,5 +47,7 @@ function dbConnect()
         return $pdo;
     } catch (PDOException $e) {
         echo 'Error: ' . $e->getMessage();
+
+        throw new PDOException($e);
     }
 }
